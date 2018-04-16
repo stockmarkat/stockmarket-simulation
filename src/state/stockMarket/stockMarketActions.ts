681acf3d9485619d3d8e1138acff70d40ca4eb8c
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-04-16 11:53:17 +0200

    buy and sell Stock action added

--- src/state/stockMarket/stockMarketActions.ts
+++ src/state/stockMarket/stockMarketActions.ts
@@ -2,6 +2,8 @@ import { Stock } from '../AppState';
 
 export const LOAD_STOCKS = 'stockMarketReducer/load-stocks';
 export const ADD_STOCKS = 'stockMarketReducer/add-stocks';
+export const BUY_STOCK = 'stockMarketReducer/buy-stock';
+export const SELL_STOCK = 'stockMarketReducer/sell-stock';
 
 export const loadStocks = () => ({
     type: LOAD_STOCKS
@@ -12,7 +14,25 @@ export const addStocks = (stocks: Stock[]): AddStocksAction => ({
     stocks
 });
 
+export const buyStock = (stockName: string, amount: number): PurchaseStockAction => ({
+    type: BUY_STOCK,
+    amount,
+    stockName
+});
+
+export const sellStock = (stockName: string, amount: number): PurchaseStockAction => ({
+    type: SELL_STOCK,
+    amount,
+    stockName
+});
+
 export interface AddStocksAction {
     type: string;
     stocks: Stock[];
+}
+
+export interface PurchaseStockAction {
+    type: string;
+    stockName: string;
+    amount: number;
 }
