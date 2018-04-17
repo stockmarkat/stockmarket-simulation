1fcc547c545aa68fcb163f95806a269bb8872069
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-04-17 13:02:35 +0200

    fixed Typo

--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -27,7 +27,7 @@ export interface Stock {
     valueChange: number; // for the last x Hours/days in %
     type: 'FireArms' | 'Energy' | 'Finance' | 'RawMaterials';
     valueHistory: FinancialSnapshot[];
-    quantity: number; // count of owend stocks
+    quantity: number; // count of owned stocks
 }
 
 export interface FinancialSnapshot {
