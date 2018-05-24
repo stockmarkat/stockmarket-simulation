d30228fb82ef7de028e6a5ce2383c6169cd36bbf
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-05-24 13:01:46 +0200

    add icons

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import './assets/css/bootstrap.min.css';
 import './assets/css/animate.min.css';
 import './assets/sass/light-bootstrap-dashboard.css';
 import './assets/css/demo.css';
+import './assets/css/pe-icon-7-stroke.css';
 
 const store = configureStore();
 
