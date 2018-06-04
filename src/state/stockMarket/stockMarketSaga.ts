8eb5c2d3522251aa7ccc571a89428f34ca821d67
Florian Rohrer <florian.rohrer@vivaldi.net>
2018-06-04 19:52:31 +0200

    StockCategory Chart => Categoriers with values of 0 are not in the store anymore

--- src/state/stockMarket/stockMarketSaga.ts
+++ src/state/stockMarket/stockMarketSaga.ts
@@ -15,23 +15,23 @@ import {
 } from './stockMarketActions';
 import { getStocks } from './stockSelector';
 
-function getRandomArbitrary(min: number, max: number) {
+function getRandomArbitrary( min: number, max: number ) {
     return Math.random() * (max - min) + min;
 }
 
-const stockJson = require('./stocks.json');
+const stockJson = require( './stocks.json' );
 
-function getNextValue(currentValue: number, volatility: number): number {
-    const random = getRandomArbitrary(0, 1);
+function getNextValue( currentValue: number, volatility: number ): number {
+    const random = getRandomArbitrary( 0, 1 );
     let changePercent = 2 * volatility * random;
-    if (changePercent > volatility) {
+    if ( changePercent > volatility ) {
         changePercent -= (2 * volatility);
     }
     const changeAmount = currentValue * changePercent;
     const nextAmount = currentValue + changeAmount;
 
-    if (nextAmount <= 0) {
-        return getNextValue(currentValue, volatility);
+    if ( nextAmount <= 0 ) {
+        return getNextValue( currentValue, volatility );
     }
     return nextAmount;
 }
@@ -40,56 +40,56 @@ function* loadinitialStocks() {
     let stocks: Stock[] = stockJson;
 
     // set Default values
-    stocks.forEach(stock => {
+    stocks.forEach( stock => {
         stock.quantity = 0;
-        stock.valueChange = getRandomArbitrary(-10, 10);
+        stock.valueChange = getRandomArbitrary( -10, 10 );
         stock.valueHistory = [];
 
-        for (let i = 720; i >= 0; i--) { // 720 = amount of 5 second blocks in the past
-            const nextValue = getNextValue(stock.value, stock.volatility / 100);
-            stock.valueHistory.push({
+        for ( let i = 720; i >= 0; i-- ) { // 720 = amount of 5 second blocks in the past
+            const nextValue = getNextValue( stock.value, stock.volatility / 100 );
+            stock.valueHistory.push( {
                 value: nextValue,
-                date: moment().subtract(i * 5, 'seconds').toDate(),
-            });
+                date: moment().subtract( i * 5, 'seconds' ).toDate(),
+            } );
             stock.value = nextValue;
         }
-    });
+    } );
 
-    yield put(addStocks(stocks));
-    yield put(calculateNextStockValues());
+    yield put( addStocks( stocks ) );
+    yield put( calculateNextStockValues() );
 }
 
-function* buyOrSellStocks(action: BuyOrSellStockAction) {
+function* buyOrSellStocks( action: BuyOrSellStockAction ) {
     // search the stock to buy
-    let stocks: Stock[] = yield select(getStocks);
-    const actionStock: Stock | undefined = stocks.find(s => s.name === action.stockName);
+    let stocks: Stock[] = yield select( getStocks );
+    const actionStock: Stock | undefined = stocks.find( s => s.name === action.stockName );
 
-    if (!actionStock) {
-        addNotification({
+    if ( !actionStock ) {
+        addNotification( {
             level: 'error',
             message: 'Stock with name ' + action.stockName + ' could not be found'
-        });
+        } );
         return;
     }
 
-    const accountValue = yield select(getAccountValue);
+    const accountValue = yield select( getAccountValue );
 
     // check if enought money
     const totalStockBuyValue = actionStock!.value * action.amount;
-    if (accountValue < totalStockBuyValue) {
-        addNotification({
+    if ( accountValue < totalStockBuyValue ) {
+        addNotification( {
             level: 'error',
             message: 'Not enough Money'
-        });
+        } );
         return;
     }
 
     // check if you can sell/buy amount of stocks
-    if (actionStock.quantity + action.amount < 0) {
-        addNotification({
+    if ( actionStock.quantity + action.amount < 0 ) {
+        addNotification( {
             level: 'error',
             message: 'Cant sell stock you dont own'
-        });
+        } );
         return;
     }
 
@@ -97,40 +97,43 @@ function* buyOrSellStocks(action: BuyOrSellStockAction) {
 
     // Buy Stocks
     // update stocks in Store
-    yield put(changeStockQuantity(action.stockName, action.amount));
-    yield put(changeAccountValue(-totalStockBuyValue));
-    yield put(changeTotalStockValue(totalStockBuyValue));
-    stocks = yield select(getStocks);
-    yield recalculateStockCategoryValues(stocks);
+    yield put( changeStockQuantity( action.stockName, action.amount ) );
+    yield put( changeAccountValue( -totalStockBuyValue ) );
+    yield put( changeTotalStockValue( totalStockBuyValue ) );
+    stocks = yield select( getStocks );
+    yield recalculateStockCategoryValues( stocks );
 }
 
-function* recalculateStockCategoryValues(stocks: Stock[]) {
+function* recalculateStockCategoryValues( stocks: Stock[] ) {
     let categoryValues: StockCategoryValue[] = [];
 
-    stocks.forEach(s => {
-        let catIndex = categoryValues.findIndex(v => v.categoryName === s.type);
-        if (catIndex === -1) {
-            categoryValues.push({categoryName: s.type, ratio: s.quantity});
+    stocks.forEach( s => {
+        let catIndex = categoryValues.findIndex( v => v.categoryName === s.type );
+        if ( catIndex === -1 ) {
+            categoryValues.push( { categoryName: s.type, ratio: s.quantity } );
         } else {
             categoryValues[ catIndex ].ratio += s.quantity;
         }
-    });
+    } );
 
-    yield put(setCategoryValues(categoryValues));
+    // remove all categoryValues without any Value
+    categoryValues = categoryValues.filter( c => c.ratio > 0 );
+
+    yield put( setCategoryValues( categoryValues ) );
 }
 
 function* calculateAllNextStockValues() {
-    const stocks: Stock[] = yield select(getStocks);
+    const stocks: Stock[] = yield select( getStocks );
 
-    stocks.forEach(s => {
+    stocks.forEach( s => {
         // TODO:
-    });
+    } );
 }
 
 function* stockMarketSaga() {
-    yield takeEvery(LOAD_STOCKS, loadinitialStocks);
-    yield takeEvery(BUY_OR_SELL_STOCKS, buyOrSellStocks);
-    yield takeEvery(CALCULATE_NEXT_STOCK_VALUES, calculateAllNextStockValues);
+    yield takeEvery( LOAD_STOCKS, loadinitialStocks );
+    yield takeEvery( BUY_OR_SELL_STOCKS, buyOrSellStocks );
+    yield takeEvery( CALCULATE_NEXT_STOCK_VALUES, calculateAllNextStockValues );
 }
 
 export default stockMarketSaga;
