5cd16ea7ef8748fbd629059d2f02efdf58d1aa78
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2018-06-28 15:26:22 +0200

    State loading correctly

--- src/state/news/newsReducer.ts
+++ src/state/news/newsReducer.ts
@@ -1,11 +1,18 @@
 import { GenericAction, NewsState } from '../AppState';
+import { ADD_NEWS, AddNewsAction } from './newsActions';
 
 const initialState: NewsState = {
     news: []
 };
 
-const newsReducer = ( state = initialState, action: GenericAction ) => {
-    switch ( action.type ) {
+const newsReducer = (state = initialState, action: GenericAction) => {
+    switch (action.type) {
+        case ADD_NEWS: {
+            const newNews = (action as AddNewsAction).quests;
+            return {
+                news: newNews
+            };
+        }
         default: {
             return state;
         }
