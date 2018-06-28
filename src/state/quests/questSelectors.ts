34eceba3e18e44a0261f889a324aeccec09fb4de
Florian Rohrer <Florian.Rohrer@vivaldi.net>
2018-06-28 15:28:52 +0200

    Active Quests selector

--- src/state/quests/questSelectors.ts
+++ src/state/quests/questSelectors.ts
@@ -3,6 +3,13 @@ import { AppState, Quest } from '../AppState';
 
 export const getQuests = (state: AppState) => state.quests.quests;
 
+export const getActiveQuests = createSelector(
+    [getQuests],
+    (quests: Quest[]) => {
+        return quests.filter(q => q.isUnlocked && !q.completed);
+    }
+);
+
 export const getUnlockedQuests = createSelector(
     [getQuests],
     (quests: Quest[]) => {
