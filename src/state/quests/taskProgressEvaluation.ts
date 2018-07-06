c4023acc3c42ddb5713d97a2c71d8b891e0d50ec
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-07-06 17:13:36 +0200

    implement glencore quest type

--- src/state/quests/taskProgressEvaluation.ts
+++ src/state/quests/taskProgressEvaluation.ts
@@ -1,22 +1,25 @@
 import { call, select } from 'redux-saga/effects';
-import { QuestTask, StockCategoryValue } from '../AppState';
+import { QuestTask, Stock, StockCategoryValue } from '../AppState';
 import { getCapital, getStockCategoryValues, getStockValue } from '../depot/depotSelector';
-import { getOwnedStocksAmount } from '../stockMarket/stockSelector';
+import { getOwnedStocksAmount, getStocks } from '../stockMarket/stockSelector';
 
-export function* getTaskProgress(task: QuestTask) {
+export function* getTaskProgress( task: QuestTask ) {
     let progress = 0;
     switch (task.questType) {
         case 'MoneyPossession':
-            progress = yield call(getMoneyPossessionTaskProgress, task);
+            progress = yield call( getMoneyPossessionTaskProgress, task );
             break;
         case 'StockTotalPossession':
-            progress = yield call(getStockTotalPossessionTaskProgress, task);
+            progress = yield call( getStockTotalPossessionTaskProgress, task );
             break;
         case 'StockInvestmentPercent':
-            progress = yield call(getStockInvestmentPercentTaskProgress, task);
+            progress = yield call( getStockInvestmentPercentTaskProgress, task );
             break;
         case 'CategoryPercentPossession':
-            progress = yield call(getCategoryPercentPossessionTaskProgress, task);
+            progress = yield call( getCategoryPercentPossessionTaskProgress, task );
+            break;
+        case 'StockPercentPossesion':
+            progress = yield call( getStockPercentPossessionTaskProgress, task );
             break;
         default:
             return 0;
@@ -29,20 +32,20 @@ export function* getTaskProgress(task: QuestTask) {
     return progress;
 }
 
-function* getStockInvestmentPercentTaskProgress(task: QuestTask) {
-    const totalCapital = yield select(getCapital);
-    const stocks = yield select(getStockValue);
+function* getStockInvestmentPercentTaskProgress( task: QuestTask ) {
+    const totalCapital = yield select( getCapital );
+    const stocks = yield select( getStockValue );
 
     const investPercent = stocks * 100 / totalCapital;
     return investPercent * 100 / task.amount;
 }
 
-function* getCategoryPercentPossessionTaskProgress(task: QuestTask) {
+function* getCategoryPercentPossessionTaskProgress( task: QuestTask ) {
     // getAll categories
-    const categories: StockCategoryValue[] = yield select(getStockCategoryValues);
+    const categories: StockCategoryValue[] = yield select( getStockCategoryValues );
 
     // search category
-    const category = categories.find(c => c.categoryName === task.typeName);
+    const category = categories.find( c => c.categoryName === task.typeName );
 
     // no stocks of this category
     if (!category) {
@@ -53,12 +56,22 @@ function* getCategoryPercentPossessionTaskProgress(task: QuestTask) {
     return category.ratio * 100 / task.amount;
 }
 
-function* getMoneyPossessionTaskProgress(task: QuestTask) {
-    const money = yield select(getCapital);
+function* getStockPercentPossessionTaskProgress( task: QuestTask ) {
+    const stocks: Stock[] = yield select( getStocks );
+    const wantedStock = stocks.find( ( stock ) => stock.name === task.typeName );
+
+    const stockQuantity = yield select( getOwnedStocksAmount );
+
+    const percentOwnedOfStock = wantedStock!.quantity / (stockQuantity / 100);
+    return percentOwnedOfStock * 100 / task.amount;
+}
+
+function* getMoneyPossessionTaskProgress( task: QuestTask ) {
+    const money = yield select( getCapital );
     return money * 100 / task.amount;
 }
 
-function* getStockTotalPossessionTaskProgress(task: QuestTask) {
-    const ownedStockAmount = yield select(getOwnedStocksAmount);
+function* getStockTotalPossessionTaskProgress( task: QuestTask ) {
+    const ownedStockAmount = yield select( getOwnedStocksAmount );
     return ownedStockAmount * 100 / task.amount;
 }
