ef591b2c74fa49be73dd7f2d7d0f3bc119eb2b91
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2018-07-07 16:02:26 +0200

    add space to categories

--- src/util/colorNormalize.ts
+++ src/util/colorNormalize.ts
@@ -3,13 +3,13 @@ import { StockType } from '../state/AppState';
 // gets the color of a category
 export const getColor = (type: StockType): string => {
     switch (type) {
-        case 'FireArms':
+        case 'Fire Arms':
             return '#EE2D20';
         case 'Energy':
             return '#87CB16';
         case 'Finance':
             return '#943bea';
-        case 'RawMaterials':
+        case 'Raw Materials':
             return '#FF9500';
         case 'Technology':
             return '#3472F7';
